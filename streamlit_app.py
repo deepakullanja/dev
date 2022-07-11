fb7a891eead5e13742aac742652756c05c0b8a3a
deepakullanja <109074963+deepakullanja@users.noreply.github.com>
2022-07-11 16:22:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega3 and Blueberry Oatmeal')
+streamlit.text('Kale,spinach')
+streamlit.text('Fruit ball')
